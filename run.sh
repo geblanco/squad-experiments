04068bf66b8591d2925097e13d6b3fc8d4e6e214
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2019-05-20 17:06:58 +0200

    Add experiment file variables setup

--- run.sh
+++ run.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# from experiment file
-# Mandatory SRVR_ADDR, SRVR_DEST_DIR
-# Mandatory BATCH_SIZE, SEQ_LENGTH
-# Optional EXP_NAME, GPUS_NO, GPUS_MODEL
-[ -f experiment ] && source  experiment
-[[ -z SRVR_ADDR || -z SRVR_DEST_DIR ]] && (echo 'No backup space'; exit 1)
+POWEROFF=${1:-0}
 
-OUTPUT_DIR=`pwd`/squad_output
-TRAIN=${1:-True}
-THRESH=${2:-'0'}
-BACKUP=${3:-0}
-POWEROFF=${4:-0}
+[ -f experiment ] && source  experiment
+
+if [[ ! -z $BACKUP && $BACKUP -eq 1 ]]; then
+  [[ -z SRVR_ADDR || -z SRVR_DEST_DIR ]] && (echo 'No backup space'; exit 1)
+fi
+
+OUTPUT_DIR=${OUTPUT_DIR:-`pwd`/squad_output}
+TRAIN=${TRAIN:-True}
+THRESH=${THRESH:-'0'}
 
 if [[ -z $DOCKERIZE ]]; then
-  time -o $OUTPUT_DIR/run_time.txt ./run_squad.sh $TRAIN $THRESH
+  time -o $OUTPUT_DIR/run_time.txt ./run_squad.sh
 else
   time -o $OUTPUT_DIR/run_time.txt (nvidia-docker run \
     -v `pwd`:/workspace \
     nvcr.io/nvidia/tensorflow:19.02-py3 \
-    /workspace/run_squad.sh $TRAIN $THRESH \
+    /workspace/run_squad.sh \
     2>&1) \
   | tee $OUTPUT_DIR/train_log
 fi
--- run_squad.sh
+++ run_squad.sh
@@ -5,16 +5,20 @@
 # Mandatory BATCH_SIZE, SEQ_LENGTH
 # Optional EXP_NAME, GPUS_NO, GPUS_MODEL, CHECKPOINT
 [ -f experiment ] && source  experiment
-[[ -z SRVR_ADDR || -z SRVR_DEST_DIR ]] && (echo 'No backup space'; exit 1)
-[[ -z BATCH_SIZE || -z SEQ_LENGTH ]] && (echo 'Need experiment variables'; exit 2)
 
-BERT_DIR=`pwd`/models/uncased
-SQUAD_DIR=`pwd`/squad
-OUTPUT_DIR=`pwd`/squad_output
+BATCH_SIZE=${BATCH_SIZE:-12}
+SEQ_LENGTH=${SEQ_LENGTH:-384}
+
+BERT_DIR=${BERT_DIR:-`pwd`/models/uncased}
+SQUAD_DIR=${SQUAD_DIR:-`pwd`/squad}
+OUTPUT_DIR=${OUTPUT_DIR:-`pwd`/squad_output}
 CHECKPOINT=${CHECKPOINT:-$BERT_DIR/bert_model.ckpt}
 
-TRAIN=${1:-True}
-THRESH=${2:-'0'}
+TRAIN=${TRAIN:-True}
+THRESH=${THRESH:-'0'}
+
+TRAIN_FILE=${TRAIN_FILE:-$SQUAD_DIR/train-v2.0.json}
+PREDICT_FILE=${PREDICT_FILE:-$SQUAD_DIR/dev-v2.0.json}
 
 cd bert
 python run_squad.py \
@@ -22,9 +26,9 @@ python run_squad.py \
   --bert_config_file=$BERT_DIR/bert_config.json \
   --init_checkpoint=$CHECKPOINT \
   --do_train=$TRAIN \
-  --train_file=$SQUAD_DIR/train-v2.0.json \
+  --train_file=$TRAIN_FILE \
   --do_predict=True \
-  --predict_file=$SQUAD_DIR/dev-v2.0.json \
+  --predict_file=$PREDICT_FILE \
   --train_batch_size=$BATCH_SIZE \
   --learning_rate=3e-5 \
   --num_train_epochs=2.0 \
