8226905974a4b33a026398858a8693aee66ad028
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2019-06-11 11:27:29 +0200

    Fix source directory on prediction script to correctly handle paths

--- predictions/predict_all.sh
+++ predictions/predict_all.sh
@@ -4,8 +4,9 @@ predict_files=$(ls *.experiment)
 for prediction in ${predict_files[@]}; do
   cd ..
   ./run.sh $prediction
-  cd -
+  # source first to do it on root folder
   source $prediction
+  cd -
   # get the results to avoid overwriting
   cp $OUTPUT_DIR/nbest_predictions.json "${prediction}_nbest_predictions.json"
   cp $OUTPUT_DIR/null_odds.json "${prediction}_null_odds.json"
