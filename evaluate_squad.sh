73ef41312564627ab5a7de95bf54153655b6fd27
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2019-06-18 11:43:10 +0200

    Better loggin

--- evaluate_squad.sh
+++ evaluate_squad.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 
 correct_thr=${1:-0}; shift
 
@@ -9,6 +8,8 @@ if [[ $# -gt 0 ]]; then
 else
   predictions=$(ls -d predictions/*/ | cut -f2 -d'/')
 fi
+
+set -e
 
 eval_script=`pwd`/squad/evaluate-v2.0.py
 for prediction in ${predictions[@]}; do
@@ -19,6 +20,7 @@ for prediction in ${predictions[@]}; do
       source $exp
       output=${prediction_data}/results.json
       # get new threshold
+      echo "$(basename $PREDICT_FILE) $(basename ${prediction_data})/predictions.json --na-prob-file $(basename ${prediction_data})/null_odds.json"
       new_thrs=$(python $eval_script $PREDICT_FILE ${prediction_data}/predictions.json --na-prob-file ${prediction_data}/null_odds.json | tee $output | grep "best_f1_thresh" | awk '{print $2}')
       if [[ $correct_thr -ne 0 ]]; then
         # remove threshold from experiment file
