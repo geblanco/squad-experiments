fb59abd988a3326a18e4fd32ca7ab578766e2a43
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2019-07-16 17:29:55 +0200

    Better documentation on upper bound script

--- scripts/upper_bound.py
+++ scripts/upper_bound.py
@@ -15,14 +15,10 @@ def parse_args():
     sys.exit(1)
   return parser.parse_args()
 
-# def upper_bound(scores):
-#   total_correct = 0
-#   for score_set in zip(*[s.values() for s in scores]):
-#     total_correct += int(max(score_set))
-#   return { 'upper_bound': 100.0 * (total_correct / len(scores[0])) }
 def upper_bound(scores):
   total_correct = 0
   for question_set in zip(*[s.items() for s in scores]):
+    # [(id, score), (id, score), ...] -> [(id, id, ...), (score, score, ...)]
     ids, score_set = list(zip(*question_set))
     if len(set(ids)) > 1:
       raise ValueError('Different question ids', ids, score_set)
