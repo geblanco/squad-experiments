6d690fc98475b760ee136c84ca41298ecddb37d3
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2019-08-30 17:37:36 +0200

    Add overlap answers measure, ToDo := Finish script
    Format plot results

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -1,14 +1,45 @@
 from matplotlib import pyplot as plt
+from collections import defaultdict
+
 import json, os, sys
 import pandas as pd
+import argparse
 
-if len(sys.argv) < 2:
-  print('Usage: plot_results.py <data dir>')
-  sys.exit(0)
-
-main_folder = sys.argv[1]
-# main_folder = 'results'
+plt.rcParams.update({'font.size': 30})
+args = None
+main_folder = None
 number_seq = ''.join([str(s) for s in list(range(10))])
+
+def parse_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-d', '--datadir', help='directory with results', required=True, type=str)
+  parser.add_argument('-f', '--filter', help='directory to put the clean data', required=False,
+                      type=str, action='store', nargs="*", default=[])
+  if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+  return parser.parse_args()
+
+def shorten_name(names):
+  names = [n.capitalize() for n in names]
+  if len(names) > 1:
+    # if more than one name, only initial letter
+    names = [n[0] for n in names]
+  return names
+
+def sort_index(index):
+  ret_index = []
+  # Sort like: Newsqa, N + S..., Squad, S + T ...
+  # group by initial letter
+  groups = defaultdict(list)
+  for i in index:
+    groups[i[0]].append(i)
+  # reorder groups
+  for group in groups.values():
+    tokens = [t.split(' ') for t in group]
+    tokens.sort(reverse=True)
+    ret_index.extend([' '.join(t) for t in tokens])
+  return ret_index
 
 def clean_name(name): 
   name_tokens = name.split('_') 
@@ -28,7 +59,7 @@ def clean_name(name):
   if name_tokens[-1] == 'epochs':
     name_tokens[-1:] = ''
   # print('Clean %s -> %s' % (name, '_'.join(name_tokens)))
-  return '_'.join(name_tokens)
+  return ' '.join(shorten_name(name_tokens))
 
 def read_model_data(model_dir):
   data_dirs = [d for d in os.listdir(model_dir) if d.endswith('_out')]
@@ -37,24 +68,37 @@ def read_model_data(model_dir):
   acc_data = { clean_name(d): data[d]['exact'] for d in data.keys() }
   return acc_data
 
-data = {}
-model_data_dirs = ['{}/{}'.format(main_folder, d) for d in os.listdir(main_folder)]
-for model_data_dir in model_data_dirs:
-  model_data = read_model_data(model_data_dir)
-  dataset_name = model_data_dir.split('/')[-1]
-  data[dataset_name] = model_data
+def main():
+  data = {}
+  dirs = os.listdir(main_folder)
+  index = ['newsqa', 'squad', 'triviaqa', 'mixed']
+  if len(args.filter) > 0:
+    dirs = [d for d in dirs if d in args.filter]
+    index = [d for d in dirs if d in args.filter]
 
-df = pd.DataFrame(data)
-df = df.reindex(['newsqa', 'squad', 'triviaqa', 'mixed'], axis=1)
-print(df)
-ax = df.plot(kind='bar')
-ax.set_xticklabels(df.index, rotation=20)
-plt.yticks(list(range(0, 101, 10)))
-plt.grid(axis='y', alpha=0.5)
+  model_data_dirs = ['{}/{}'.format(main_folder, d) for d in dirs]
+  for model_data_dir in model_data_dirs:
+    model_data = read_model_data(model_data_dir)
+    dataset_name = model_data_dir.split('/')[-1]
+    data[dataset_name] = model_data
 
-# to get a max over each dataset:
-# df.max()
-# to get the rows with the max values
-# df.loc[df.idxmax()]
+  df = pd.DataFrame(data)
+  df = df.reindex(index, axis=1)
+  df = df.reindex(sort_index(df.index))
+  print(df)
+  ax = df.plot(kind='bar')
+  ax.set_xticklabels(df.index, rotation='horizontal')
+  plt.yticks(list(range(0, 101, 10)))
+  plt.grid(axis='y', alpha=0.5)
 
-plt.show()
+  # to get a max over each dataset:
+  # df.max()
+  # to get the rows with the max values
+  # df.loc[df.idxmax()]
+
+  plt.show()
+
+if __name__ == '__main__':
+  args = parse_args()
+  main_folder = args.datadir
+  main()
