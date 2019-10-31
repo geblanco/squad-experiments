03e33c689346c5994910e9863c8fa476e83b7b13
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2019-10-31 13:21:32 +0100

    Add print stats filelist as argument

--- scripts/print_stats.sh
+++ scripts/print_stats.sh
@@ -2,13 +2,19 @@
 
 set -e 
 
-for dir in $(cat filelist); do
+if [[ $# -lt 1 ]]; then
+  echo "Usage ./print_stats.sh <filelist>"
+  exit 0
+fi
+
+experiments=$(cat $1)
+for dir in ${experiments[@]}; do
   exp_file=${dir%_out}.experiment
   if [[ ! -f $exp_file ]]; then
     exp_family_name=$(dirname $exp_file)
     exp_family_name=$(basename $exp_family_name)
     exp_name=$(basename $exp_file)
-    exp_dir_name=experiments/$exp_family_name/$exp_name
+    exp_dir_name="experiments/$exp_family_name/$exp_name"
     echo $exp_family_name $exp_name $exp_dir_name
     if [[ -f $exp_dir_name ]]; then
       cp $exp_dir_name $exp_file
