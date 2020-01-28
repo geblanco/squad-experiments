3242d42c39fce990da60ba443365db294b6770fb
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-01-28 16:25:26 +0100

    Add empty proportions in dataset stats script

--- scripts/dataset_stats.py
+++ scripts/dataset_stats.py
@@ -24,24 +24,35 @@ def stddev(group):
   return sqrt(sum(s)/(len(group)-1))
 
 def main():
-  len_paragraphs = []
-  len_questions = []
-  len_answers = []
+  paragraphs = []
+  questions = []
+  answers = []
+  nb_empty_answers = 0
   for entry in dataset:
     for paragraph in entry['paragraphs']:
-      len_paragraphs.append(len(paragraph['context']))
+      paragraphs.append(len(paragraph['context']))
       for qa in paragraph['qas']:
-        len_questions.append(len(qa['question']))
+        questions.append(len(qa['question']))
         if qa['answers']:
-          len_answers.extend([len(a['text']) for a in  qa['answers']])
-  print('Number of paragraphs {}'.format(len(len_paragraphs)))
-  print('Avg paragraph len {:.4} ± {:.4}'.format(mean(len_paragraphs),
-    stddev(len_paragraphs)))
-  print('Number of questions {}'.format(len(len_questions)))
-  print('Avg question len {:.4} ± {:.4}'.format(mean(len_questions),
-    stddev(len_questions)))
-  print('Avg answer len {:.4} ± {:.4}'.format(mean(len_answers),
-    stddev(len_answers)))
+          answers.extend([len(a['text']) for a in  qa['answers']])
+        if qa.get('is_impossible', False) or len(qa['answers']) == 0:
+          nb_empty_answers += 1
+  len_paragraphs = len(paragraphs)
+  mean_par = mean(paragraphs)
+  stddev_par = stddev(paragraphs)
+  len_questions = len(questions)
+  mean_ques = mean(questions)
+  stddev_ques = stddev(questions)
+  len_answers = len(answers)
+  mean_ans = mean(answers)
+  stddev_ans = stddev(answers)
+  print('Number of paragraphs {}'.format(len_paragraphs))
+  print('Avg paragraph len {:.4} ± {:.4}'.format(mean_par, stddev_par))
+  print('Number of questions {}'.format(len_questions))
+  print('Avg question len {:.4} ± {:.4}'.format(mean_ques, stddev_ques))
+  print('Avg answer len {:.4} ± {:.4}'.format(mean_ans, stddev_ans))
+  print('Number of empty answer questions {}'.format(nb_empty_answers))
+  print('% of empty answer questions {}'.format(nb_empty_answers/len_questions))
 
 if __name__ == '__main__':
   args = parse_args()
